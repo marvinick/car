45affac286a14e6141da3fcadb432263cb960cbe
Jason Creighton & Kenny Grage <kennygrage@gmail.com>
2015-08-03 12:09:42 -0700

    getter and setter

--- car.php
+++ car.php
@@ -4,30 +4,60 @@ class Car
 {
 
   public $make_model;
-  public $price;
+  private $price;
   public $miles;
+
+  function __construct($make_model, $price, $miles) {
+    $this->make_model = $make_model;
+    $this->price = $price;
+    $this->miles = $miles;
+
+  }
+
+  function getPrice() {
+    return $this->price;
+  }
+
+  function setPrice($new_price) {
+    $float_price = (float) $new_price;
+    if ($float_price !=0) {
+      $formatted_price = number_format($float_price, 2);
+      $this->price = $formatted_price;
+    }
+  }
 
 }
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
 
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
 
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
+$porsche = new Car("2014 Porsche 911", 114991, 7864);
+$ford = new Car("2011 Ford F450", 55995, 14241);
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+$mercedes->setPrice("35000.125");
+$porsche->setPrice("hotdog");
 
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
+
+// $porsche = new Car();
+// $porsche->make_model = "2014 Porsche 911";
+// $porsche->price = 114991;
+// $porsche->miles = 7864;
+//
+// $ford = new Car();
+// $ford->make_model = "2011 Ford F450";
+// $ford->price = 55995;
+// $ford->miles = 14241;
+//
+// $lexus = new Car();
+// $lexus->make_model = "2013 Lexus RX 350";
+// $lexus->price = 44700;
+// $lexus->miles = 20000;
+//
+// $mercedes = new Car();
+// $mercedes->make_model = "Mercedes Benz CLS550";
+// $mercedes->price = 39900;
+// $mercedes->miles = 37979;
+
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
 
@@ -40,9 +70,10 @@ $cars = array($porsche, $ford, $lexus, $mercedes);
   <div class="container">
     <?php
       foreach ($cars as $specific_car) {
+        $car_price = $specific_car->getPrice();
         echo "<p>$specific_car->make_model</p>
               <p>$specific_car->miles miles</p>
-              <p>$$specific_car->price</p>
+              <p>$$car_price</p>
         ";
 
       }
